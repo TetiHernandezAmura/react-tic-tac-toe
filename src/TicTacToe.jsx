8ccc1ddfb423b7d228a484dc808c0807de734d52
Gilberto Hernandez Amura <gilbertohernandezamura@gmail.com>
2023-08-27 16:19:04 -0300

    final clean up

--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -3,21 +3,19 @@ import "./TicTacToe.css";
 import Board from "./Board";
 
 const TicTacToe = () => {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   const handlePlay = (nextSquares) => {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   };
 
   const jumpTo = (nextMove) => {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   };
 
   const moves = history.map((_, move) => {
